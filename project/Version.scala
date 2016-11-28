7e7c417fd86e1ea7fe363b1c08a3a60c0159f807
Kevin van der Vlist <kvdvlist@sogyo.nl>
2016-11-28 13:26:54 +0100

    Scala 2.12 support

--- project/Version.scala
+++ project/Version.scala
@@ -1,7 +1,7 @@
 object Version {
   val project = "0.6.0"
-  val scala = "2.11.7"
-  val akka = "2.4.4"
-  val play = "2.5.2"
-  val rediscala = "1.6.0"
+  val scala = "2.12.0"
+  val akka = "2.4.12"
+  val play = "2.5.10"
+  val rediscala = "1.8.0-SNAPSHOT"
 }
